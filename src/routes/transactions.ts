cbc39dee2e05557002b8d9aeb05a96677f575b76
gitirana <thaygitirana@gmail.com>
2023-07-04 11:07:02 -0300

    create get transaction by id route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -10,6 +10,18 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return { transactions };
   });
 
+  app.get("/:id", async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionsParamsSchema.parse(request.params);
+
+    const transaction = await knex("transactions").where("id", id).first();
+
+    return { transaction };
+  });
+
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
